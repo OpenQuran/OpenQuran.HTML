3bf59eac18b79950f40c5db9649fe019d9e2d9f5
aziz.kerim <aziz.kerim@a0bcad23-2a2e-0410-b4c8-b329361a1dc2>
2007-05-16 15:53:15 +0000

    - Added stand-alone cmd-line parser, so it can be unittested. - Revised cmd-line parser unittest. - Added two functions that return the argument vector in UTF-16 or UTF-8. - Added GetArgvW(). It returns the argument vector in one whole memory chunk, which makes freeing it very easy.

--- trunk/src/CmdLine.d
+++ trunk/src/CmdLine.d
@@ -4,7 +4,8 @@
 */
 module CmdLine;
 
-import std.c.stdlib : calloc, realloc, free;
+import std.c.stdlib : calloc, realloc, free, malloc;
+import std.c.string : memcpy;
 
 version(Windows)
 {
@@ -49,34 +50,105 @@ Lerr_free_dest:
 Lerr:
   return null;
 }
+
+/++
+  Get argument vector in UTF-16. Garbage collected.
++/
+wchar[][] GetUnicodeArgvW()
+{
+  wchar[][] result;
+  wchar[][] argv = GetArgvW(GetCommandLineW());
+  scope(exit) free(argv.ptr);
+  foreach (arg; argv)
+  {
+    result ~= arg.dup;
+  }
+  return result;
+}
+import std.utf;
+/++
+  Get argument vector in UTF-8. Garbage collected.
++/
+char[][] GetUnicodeArgv()
+{
+  char[][] result;
+  wchar[][] argv = GetArgvW(GetCommandLineW());
+  scope(exit) free(argv.ptr);
+  foreach (arg; argv)
+  {
+    result ~= toUTF8(arg);
+  }
+  return result;
+}
+
+} //version(Windows)
+
+int countArguments(wchar* cmdLine)
+{
+  int count;
+  alias cmdLine p;
+
+  while(*p == ' ' || *p == '\t') ++p; 
+  if (!*p)
+    return 0;
+
+  int inQuote, bsCount;
+  while(*p)
+  {
+    switch(*p)
+    {
+      case '\\':
+        bsCount = 0;
+        do
+          ++bsCount;
+        while (*++p == '\\')
+
+        if (*p == '"' && bsCount & 1)
+          ++p;
+        break;
+      case '"':
+        inQuote ^= 1;
+        ++p;
+        break;
+      case ' ','\t':
+        if (!inQuote)
+          ++count;
+        do ++p; while(*p == ' ' || *p == '\t')
+        break;
+      default:
+        ++p;
+    }
+  }
+
+  return count + 1;
 }
 
 /++
-  Author: Aziz Köksal<br><br>
   Parses the command line string returned by GetCommandLineW().
   Escape rules:
-  <pre>
-  1. '"' [^"]* '"'   -> [^"]* contents of the quote
-  2. '\'{2N} + '"'   -> '\'{N} half the number of bs + argument delimiting '"'
-  3. '\'{2N-1} + '"' -> '\'{N} + '"' half the number of bs + literal '"'
-  4. '\'{N} + [^"]   -> '\'{N} add N literal '\'
-  </pre>
+  <ol>
+  <li>'"' [^"]* '"'   -> [^"]* contents of the quote</li>
+  <li>'\'{2N} + '"'   -> '\'{N} half the number of bs + argument delimiting '"'</li>
+  <li>'\'{2N-1} + '"' -> '\'{N} + '"' half the number of bs + literal '"'</li>
+  <li>'\'{N} + [^"]   -> '\'{N} add N literal '\'</li>
+  </ol>
   Returns:
-  Array of wide strings allocated with calloc().
-  ---
-  wchar[][] argv = ParseCommandLineArgs(GetCommandLineW());
-  // ...
-  free(argv[0].ptr); // free the parsed cmd-line first
-  free(argv.ptr); // then the array itself
-  ---
-  See also: Parsing C Command-Line Arguments: http://msdn2.microsoft.com/en-us/library/ms880421.aspx
-  TODO: argv[0] should always contain the full path to the executable.
+    A string - allocated with malloc() - containing the zero-terminated arguments.
+    The parameter argc is set to the number of arguments found in cmdLine.<br>
+  See_Also: Parsing C Command-Line Arguments: http://msdn2.microsoft.com/en-us/library/ms880421.aspx
 +/
-wchar[][] ParseCommandLineArgs(wchar* cmdLine)
+wchar[] GetParsedCmdLine(wchar* cmdLine, out int argc)
+out(result)
+{
+  if (result !is null)
+    assert(result[$-1] == 0);
+}
+body
 {
   if (!cmdLine || *cmdLine == 0)
     goto Lerr;
 
+  // Determine length of cmdLine
   int cmdLineLength;
   wchar* p = cmdLine;
   while(*p++) ++cmdLineLength;
@@ -85,139 +157,205 @@ wchar[][] ParseCommandLineArgs(wchar* cmdLine)
 
   // Skip leading spaces
   while(*p == ' ' || *p == '\t') ++p;
-
-  if (*p == 0)
+  if (!*p)
     goto Lerr;
 
   // Allocate memory for the parsed cmd line.
-  wchar* dest = cast(wchar*) calloc(wchar.sizeof, cmdLineLength * wchar.sizeof);
-  if (!dest)
+  wchar* line = cast(wchar*) malloc((cmdLineLength + 1) * wchar.sizeof);
+  if (!line)
     goto Lerr;
-  wchar* argBegin = dest; // Points to the beginning of an argument.
-  int argvCapacity = 2; // Initial capacity of argv.
-  // Allocate memory for the argument vector.
-  wchar[]* argv = cast(wchar[]*) calloc((wchar[]).sizeof, argvCapacity * (wchar[]).sizeof);
-  if (!argv)
-    goto Lerr_free_dest;
-  // Number of arguments in argv.
-  int argc;
 
-  int push_back_argv()
+  alias cmdLine lineBegin; // reuse variable
+  lineBegin = line;
+  alias cmdLineLength bsCount; // reuse
+  argc = 0;
+  int inQuote;
+  while(*p)
   {
-    // Allocate more space if needed
-    if (argc == argvCapacity)
-    {
-      argvCapacity *= 2;
-      argv = cast(wchar[]*) realloc(argv, argvCapacity * (wchar[]).sizeof);
-      if (!argv)
-        return 0;
-    }
-    // Terminate argument?
-//     *dest = 0;
-//     ++dest;
-
-    // Push new argument on the vector
-    argv[argc] = argBegin[0..dest - argBegin];
-    ++argc;
-    return 1;
-  }
-  void parse_escape_sequence()
-  {
-    // Enter function if *p == '\\'
-    int bsCount; // back slash count
-    // Count the number of slashes and copy them to dest.
-    do
-    {
-      ++bsCount;
-      *dest++ = *p++;
-    } while (*p == '\\')
-
-    // If back slashes are followed by a dbl quote
-    // special rules apply.
-    if (*p == '"')
-    {
-      dest -= bsCount / 2;
-      if (bsCount & 1)
-      { // Replace previous backslash with literal '"'.
-        *(dest-1) = '"';
-        ++p;
-      }
-    }
-  }
-  // Main parse loop
-  while (*p)
-  {
-    switch (*p)
+    switch(*p)
     {
       case '\\':
-        parse_escape_sequence();
+        bsCount = 0;
+        do // Count back slashes.
+        {
+          ++bsCount;
+          *line++ = *p++;
+        } while (*p == '\\')
+
+        if (*p == '"')
+        {
+          line -= bsCount / 2;
+          if (bsCount & 1)
+          { // Replace previous back slash with a literal '"'.
+            *(line-1) = '"';
+            ++p;
+          }
+        }
         break;
       case '"':
-        while (*++p != '"')
-        {
-          wchar wc = *p;
-          if (wc == '\\')
-          { // p_e_s() sets p to the character next to the sequence.
-            parse_escape_sequence();
-            --p; // so we go back one
-            continue;
-          }
-          else
-          if (wc == 0)
-            // no closing quote found, but argument still accepted
-            goto Lend_main_loop;
-
-          *dest = wc;
-          ++dest;
-        }
-        assert(*p == '"');
+        inQuote ^= 1;
         ++p;
         break;
-      case ' ', '\t':
-        if (!push_back_argv())
-          goto Lerr_free_dest;
-        // Skip any further spaces
-        do ++p; while(*p == ' ' || *p == '\t')
-        argBegin = dest;
-        break;
+      case ' ','\t':
+        if (!inQuote)
+        {
+          do ++p; while(*p == ' ' || *p == '\t')
+          // Only increment if we didn't skip trailing spaces.
+          if(*p)
+          {
+            ++argc;
+            *line = 0;
+            ++line;
+          }
+          break;
+        }
+        // We are in a quote, so fall through to default and copy the space char.
       default:
-        *dest++ = *p++;
+        *line++ = *p++;
     }
   }
-Lend_main_loop:
+  ++argc;
+  *line = 0;
+  ++line;
 
-  if (!push_back_argv())
-    goto Lerr_free_dest;
-
-  return argv[0..argc];
-
-Lerr_free_dest:
-  free(dest);
+  return lineBegin[0..line-lineBegin];
 Lerr:
   return null;
 }
 
+/++
+  Get the argument vector in UTF-16.<br>
+  argv[0] is replaced by the fully qualified file name of the executable.
+  Returns:
+    Array of wide strings allocated with malloc().
+  ---
+  wchar[][] argv = GetArgvW(GetCommandLineW());
+  // ...
+  free(argv.ptr); // free the vector when done.
+  ---
++/
+wchar[][] GetArgvW(wchar* cmdLine)
+{
+  int argc;
+  wchar[] parsedLine = GetParsedCmdLine(cmdLine, argc);
+  if (!parsedLine || !argc)
+    goto Lerr;
+
+  wchar[] execName = GetExecutableFileName();
+  if (!execName)
+    goto Lerr_mem_free1;
+
+  // Allocate a single block of memory for all 3 parts.
+  wchar[][] argv = (cast(wchar[]*) malloc(
+    argc * (wchar[]).sizeof) + // for argv
+    parsedLine.length * wchar.sizeof + // arguments
+    execName.length * wchar.sizeof // executable name
+  )[0..argc];
+
+  if (!argv.ptr)
+    goto Lerr_mem_free2;
+
+  // Copy parsed cmd-line.
+  wchar* p = cast(wchar*) memcpy(argv.ptr + argv.length * (wchar[]).sizeof,
+    parsedLine.ptr,
+    parsedLine.length * wchar.sizeof
+  );
+  // Copy executable name.
+  wchar[] eName = (cast(wchar*) memcpy(p + parsedLine.length * wchar.sizeof,
+    execName.ptr,
+    execName.length * wchar.sizeof
+  ))[0..execName.length];
+  // Free parsed cmd-line and executable name.
+  free(parsedLine.ptr);
+  free(execName.ptr);
+
+  // Assign arguments to the vector.
+  for(int i; i < argc; ++i)
+  {
+    wchar* arg = p;
+    while(*p++) {}
+    argv[i] = arg[0..p - arg -1];
+  }
+
+  // Replace first argument with the fully qualified path to the executable.
+  argv[0] = eName;
+
+  free(execName.ptr);
+
+  return argv;
+Lerr_mem_free2:
+  free(execName.ptr);
+Lerr_mem_free1:
+  free(parsedLine.ptr);
+Lerr:
+  return null;
+}
+
+import std.stdio;
 unittest
 {
-  writefln("ParseCommandLineArgs unittest.");
+  writefln("GetParsedCmdLine unittest.");
+  // Test command-lines
   wchar[][] cmdLines = [
-    ""w, // empty string -> null
-    " \t ", // only spaces -> null
-    "main.exe", // [main.exe]
-    "  \tC:\\bla", // [C:\bla]
-    "bla \"arg1 ", // unclosed quote
-    "\"abc \"\"\" def\"", // "abc """ def" -> [abc ,def]
-    "abc \"", // abc " -> [abc,]
-    "bla \"\"\"\"\" asd asd\"", // bla """"" asd asd" -> [bla, asd asd]
-    "bla \"\"\"abc\"\"\"", //bla """abc""" -> [bla,abc]
-    "bla \"\"abc\"\"", // bla ""abc"" -> [bla,abc]
-    "\"binary\"\" \"\"with\"\" \"\"spaces.exe\"", //"binary"" ""with"" ""spaces.exe" -> [binary with spaces.exe]
-    "C:\\blubb.exe arg1 arg2", // C:\blubb.exe par1 par2 -> [C:\blubb.exe,arg1,arg2]
-    "C:\\dmd\"\\bin\"\"\"\\\\\"dmd.exe\" arg1 arg2", // [C:\dmd\bin\dmd.exe arg1 arg2]
-    "C:\\bla.exe arg\\1 arg\\\\2", // [C:\bla.exe arg\1 arg\2]
-    "hello.exe \".\\ha\\\\\" arg2" // [hello.exe,.\ha\,arg2]
+    ""w,
+    " \t ",
+    "main.exe",
+    "  \tC:\\bla   ",
+    `bla "arg1 `,
+    `"abc """ def"`,
+    `abc "`,
+    `bla """"" asd asd"`,
+    `bla """abc"""`,
+    `bla ""abc""`,
+    `"binary"" ""with"" ""spaces.exe"`,
+    `C:\blubb.exe arg1 arg2`,
+    `hello.exe ".\ha\\" arg2`,
+    `C:\bla.exe arg\1 arg\\2`,
+    `C:\dmd"\bin"""\\"dmd.exe" a\"rg1 arg2\"\\ \"ar\\\"\g3\"`
   ];
-  // TODO: returned argv is not checked and not freed.
-  foreach(line; cmdLines)
-    std.stdio.writefln(ParseCommandLineArgs(line.ptr));
+  // Array of argument vectors.
+  wchar[][][] cmdArgvs= [
+    cast(wchar[][])[],
+    [],
+    ["main.exe"w],
+    [`C:\bla`w],
+    ["bla"w, "arg1 "],
+    ["abc "w, "def"],
+    ["abc"w, ""],
+    ["bla"w, " asd asd"],
+    ["bla"w, "abc"],
+    ["bla"w, "abc"],
+    ["binary with spaces.exe"w],
+    [`C:\blubb.exe`w, "arg1", "arg2"],
+    ["hello.exe"w, `.\ha\`, "arg2"],
+    [`C:\bla.exe`w, `arg\1`, `arg\\2`],
+    [`C:\dmd\bin\dmd.exe`w, "a\"rg1", `arg2"\\`, `"ar\"\g3"`]
+  ];
+  assert(cmdLines.length == cmdArgvs.length);
+
+  foreach(i, cmdLine; cmdLines)
+  {
+    wchar[][] cmpArgv = cmdArgvs[i];
+    int argc;
+    wchar[] parsedLine = GetParsedCmdLine(cmdLine.ptr, argc);
+    scope(exit) free(parsedLine.ptr);
+
+    if (cmpArgv.length != argc)
+      throw new Exception("Number of arguments do not match!");
+    wchar* p = parsedLine.ptr;
+    if (p)
+      for(int j; j < argc; ++j)
+      {
+        wchar* arg = p;
+        while(*p++) {} // Move to the end of the argument
+        if (cmpArgv[j] != arg[0..p - arg -1])
+        {
+          char[] err = std.string.format(
+            "Cmd-line:>%s<\n", cmdLine,
+            "Parsed:>%s< != Expected:>%s<\n", arg[0..p - arg -1], cmpArgv[j]
+          );
+          throw new Exception("Mismatch between parsed argument and expected argument:\n" ~ err);
+        }
+      }
+  }
 }
